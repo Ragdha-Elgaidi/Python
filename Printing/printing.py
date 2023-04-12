4c3f9fc66cbe95c19d51320ec7894e7a197d2135
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-04-12 09:08:34 +0200

    Create printing.py

---
+++ Printing/printing.py
@@ -0,0 +1,33 @@
+print('Hello World')
+print("Hello World")
+
+print('Hello')
+print('World')
+print('I am Ragdha')
+
+print("Hello\nWorld\nI am Ragdha")
+
+print('Hello'
+      ' World'
+      ' I am Ragdha')
+
+print(1+2+3)#6
+print(3*4)#12
+print(6/2)#3.0
+print('6/2')#6/2
+"""
+Hello World
+Hello World
+Hello
+World
+I am Ragdha
+Hello
+World
+I am Ragdha
+Hello World I am Ragdha
+6
+12
+3.0
+6/2
+
+"""
