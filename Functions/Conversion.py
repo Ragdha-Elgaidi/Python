a8d19ce15941bd823dbb221b7fd1e44328b18a10
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-04-13 05:09:31 +0200

    Create Conversion.py

---
+++ Functions/Conversion.py
@@ -0,0 +1,25 @@
+my_str = '10'
+x_int = int(my_str)
+x_float = float(my_str)
+
+print(my_str, type(my_str))
+print(x_int, type(x_int))
+print(x_float, type(x_float))
+
+my_float = 20.7
+my_int = int(my_float)
+my_str = str(my_float)
+
+print(my_float, type(my_float))
+print(my_int, type(my_int))
+print(my_str, type(my_str))
+
+'''
+10 <class 'str'>
+10 <class 'int'>
+10.0 <class 'float'>
+
+20.7 <class 'float'>
+20 <class 'int'>
+20.7 <class 'str'>
+'''
