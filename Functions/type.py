c14314961f98db3450242e9a2e01f6b091302517
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-04-13 04:58:48 +0200

    Create type.py

---
+++ Functions/type.py
@@ -0,0 +1,15 @@
+'''
+we can use _ with int and float to write numbers in easier way
+'''
+age = 1000_000_000
+
+print(type(15))
+print(type(age))
+print(type(20.5))
+print(type('20.5'))
+"""
+<class 'int'>
+<class 'int'>
+<class 'float'>
+<class 'str'>
+"""
