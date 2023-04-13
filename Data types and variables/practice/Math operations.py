7fd8aa0ba0c6f354d322fe7e6efa895b6351b91e
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-04-13 23:47:19 +0200

    Create Math operations.py

---
+++ Data types and variables/practice/Math operations.py
@@ -0,0 +1,13 @@
+first = input('Enter first number: ')
+first = float(first)
+
+# let's make it in a single line
+second = float(input('Enter second number: '))
+
+print(first, '+', second, '=', first + second)
+print(first, '-', second, '=', first - second)
+print(first, '*', second, '=', first * second)
+print(first, '/', second, '=', first / second)
+
+print("\nEnd of program")
+
