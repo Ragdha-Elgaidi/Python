265bcccd77d2992128ceea0065453a9f6cc9592d
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-04-13 04:44:21 +0200

    Create String Manipulation.py

---
+++ Data types and variables/String Manipulation.py
@@ -0,0 +1,22 @@
+print('I am Ragdha')
+print('I am'+' '+'Ragdha')
+
+str1 = 'I am'
+str2 = ' Ragdha'
+
+print(str1+str2)
+print(str1*3)
+print(2*str1+str2)
+
+str1 = 'Hello '
+str3 = str1 + str2 + str1
+
+print(str3)
+'''
+I am Ragdha
+I am Ragdha
+I am Ragdha
+I amI amI am
+I amI am Ragdha
+Hello  RagdhaHello 
+'''
