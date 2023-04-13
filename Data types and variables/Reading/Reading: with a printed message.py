c9619244e4ae74b5abddd768bc940bb22e68a7d8
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-04-13 22:04:36 +0200

    Create Reading: with a printed message.py

---
+++ Data types and variables/Reading/Reading: with a printed message.py
@@ -0,0 +1,11 @@
+str = input('Enter your name: ')
+print('Hello ' + str)
+
+print('Hello '+input('Enter your name: '))
+
+'''
+Enter your name: Ragdha
+Hello Ragdha
+Enter your name: Ragdha
+Hello Ragdha
+'''
