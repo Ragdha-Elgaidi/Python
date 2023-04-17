9e76b32d02dfc9c3872078a0693a66a729b1f90a
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-04-18 01:42:38 +0200

    Create Our remainder.py

---
+++ Operators/Modulus Operator/Our remainder.py
@@ -0,0 +1,7 @@
+"""
+● We know N % M computes the remainder of division
+● Write a program that reads 2 positive integers and print such reminder without using the modulus operator %
+"""
+n, m = map(int, input().split())
+
+print(n % m)
